318ced697feb514a01cbced1949b77164b6128d7
nihadbellatreche <123741993+nihadbellatreche@users.noreply.github.com>
2025-04-25 11:51:36 +0100

    pages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar"; // Import the Navbar component
 import SidebarAndNavbar from './components/SidebarAndNavbar';
 import RemarqueCard from './components/RemarqueCard';
@@ -9,10 +10,19 @@ import TerrainTable from './components/TerrainTable';
 import Home from './components/Home';
 import Filesview from './components/Filesview';
 import Notifications from './components/Notifications';
+import Dashboardsview from './components/Dashboardsview';
 function App() {
   return (
     <div className="App">
-      <Notifications/>
+      <Routes>
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/TerrainTable" element={<TerrainTable />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Filesview" element={<Filesview />} />
+        <Route path="/Notifications" element={<Notifications />} />
+        <Route path="/Dashboardsview" element={<Dashboardsview />} />
+      </Routes>
+
     </div>
   );
 }
